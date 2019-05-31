6480cd47b5fb2eeba0ab381f5e664ebe15c3ee4d
Krystian Konecki <krystian_konecki@wp.pl>
2019-05-31 14:08:55 +0200

    Adding media and portfolio section

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 //Nav
 
 const activeList = ["header", "header__nav"]
-const buttonList = [".nav__link--home", ".nav__link--services", ".nav__link--about", ".nav__link--portfolio", 
-                    ".nav__link--pricing", ".nav__link--blog", ".nav__link--contact"];
+const buttonList = [".nav__link--home", ".nav__link--services", ".nav__link--about", ".nav__link--portfolio",
+    ".nav__link--pricing", ".nav__link--blog", ".nav__link--contact"
+];
 
 document.querySelector(".header__icon-bars").addEventListener("click", function () {
     for (let i = 0; i < activeList.length; i++) {
@@ -10,10 +11,23 @@ document.querySelector(".header__icon-bars").addEventListener("click", function 
     }
 })
 
-for (let j=0; j<buttonList.length; j++){
-    document.querySelector(buttonList[j]).addEventListener('click',function(){
-        for(let k=0; k<activeList.length; k++){
-        document.querySelector("."+ activeList[k]).classList.remove(activeList[k] + '--active');
+for (let j = 0; j < buttonList.length; j++) {
+    document.querySelector(buttonList[j]).addEventListener('click', function () {
+        for (let k = 0; k < activeList.length; k++) {
+            document.querySelector("." + activeList[k]).classList.remove(activeList[k] + '--active');
         }
     })
+}
+
+//Portfolio
+const portList = ["one", "two", "three", "four", "five", "six", "seven", "eight"]
+
+for (let i = 0; i < portList.length; i++) {
+    document.querySelector(".portfolio__wrap__image--" + portList[i]).addEventListener("click", function () {
+        document.querySelector(".portfolio__wrap__container--" + portList[i]).classList.add("portfolio__wrap__container--active")
+    })
+    document.querySelector(".portfolio__container__center--" + portList[i]).addEventListener("click", function () {
+        document.querySelector(".portfolio__wrap__container--" + portList[i]).classList.remove("portfolio__wrap__container--active")
+    })
+
 }
